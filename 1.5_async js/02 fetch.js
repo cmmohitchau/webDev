5943b08095cca439478aa4c62515facbd19e2d5f
Mohit Chaudhary <cmmohitchau@gmail.com>
2024-07-23 22:10:48 +0545

    add modified file

--- 1.5_async js/02 fetch.js
+++ 1.5_async js/02 fetch.js
@@ -8,3 +8,15 @@ fs.readFile(path, "utf-8" , function(err,data){
 })
 
 console.log("hi there");
+
+async function main() {
+    const response = await fetch("https://sum-server.100xdevs.com/todos");
+    const data = await response.json()
+    const todo = data.todos;
+    console.log(todo.length);
+    todo.map( (t) => console.log(t))
+    todo.map( (t) => console.log(t.id))
+    
+}
+
+main()
